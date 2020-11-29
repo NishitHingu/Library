1715b08984b71c99852115883e2d8800a85864cc
nishit hingu <nishithingu@gmail.com>
2020-11-29 10:16:10 +0530

    added the edit, remove and responsive features.

--- main.js
+++ main.js
@@ -6,13 +6,22 @@ const add = document.querySelector(".btn-add");
 const closeBtn = document.querySelector(".close");
 const submit = document.querySelector(".submit");
 const cardStack = document.querySelector(".card-stack");
+let n = 0;
+let edit;
+let enable = 1;
+// Book constructor
+function book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 
-// Hidding the form to add books 
+// Hidding the form 
 layout.removeChild(formWrapper);    
 
 // Creating/getting the Library array in which we will store our data
 let myLibrary = [];
-let n = 0;
 if(localStorage.length == 0){
     let mybook = new book("Hobbit","J.J.R Tolkin",299,"Completed");
     myLibrary.push(mybook);
@@ -29,17 +38,13 @@ else{
 }
 
 // EventListeners
-closeBtn.addEventListener("click", () => {layout.removeChild(formWrapper);});
+closeBtn.addEventListener("click", () => {
+    layout.removeChild(formWrapper);
+    clearForm();
+    enable = 1;
+});
 add.addEventListener("click", () => {layout.appendChild(formWrapper);});
 submit.addEventListener("click",addBookToLibrary,false);
-
-// Book constructor
-function book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
 
 function addBookToLibrary(e){
     e.preventDefault();
@@ -51,21 +56,47 @@ function addBookToLibrary(e){
     if(read === null){
         read = "";
     }
-    for(let i of myLibrary){
-        if(i.title.toLowerCase() == title.toLowerCase()){
-            alert("This book is already present in the library");
-            return;
-        }
-    }
-    if(title == "" || author === "" || pages === ""){
+    if(title == "" || author == "" || pages == ""){
         alert("Please fill all the required data");
         return; 
     }
-    let info = new book(title,author,pages,read);
-    myLibrary.push(info);
-    AddToLocalStorage(info);
+    if(enable){
+        for(let i of myLibrary){
+            if(i.title.toLowerCase() == title.toLowerCase() && i.author.toLowerCase() == author.toLocaleLowerCase()){
+                console.log(enable);
+                alert("This book is already present in the library");
+                return;
+            }
+        }
+        let info = new book(title,author,pages,read);
+        myLibrary.push(info);
+        AddToLocalStorage(info);
+    }
+    else{
+        let change;
+        let info = edit.split("|");
+        let title = info[0];
+        let author = info[1];
+        console.log(title + " " + author);
+        myLibrary = myLibrary.filter(i => i.title != title && i.author != author);
+        myLibrary.push(new book(title,author,pages,read));
+        for(let i = 0; i < n; i++){
+            let key = localStorage.key(i);
+            let value = localStorage.getItem(key);
+            let data = value.split("/");
+            if(title.toLowerCase() == data[0].toLowerCase() && author.toLowerCase() == data[1].toLowerCase()){
+                console.log(key);
+                change = key.slice(4);
+                break;
+            }
+        }
+        console.log(change);
+        let push = "book" + change;
+        let value = title + "/" +  author + "/" + pages + "/" + read;
+        localStorage.setItem(push, value);
+    }
     location.reload();
-}
+;}
 
 // Adding the book info to the local Storage
 function AddToLocalStorage(info){
@@ -76,7 +107,6 @@ function AddToLocalStorage(info){
 }
 
 function clearForm(){
-    console.log("working");
     let inputs = form.querySelectorAll('input[type = "text"]');
     inputs.forEach(i => i.value = "");
     form.querySelector('input[type = "number"]').value = "";
@@ -96,8 +126,7 @@ book.prototype.display = function() {
     author.classList.add("author");
     pages.classList.add("pages");
     pgWrap.classList.add("pg-wrap");
-    pg.setAttribute("style", "text-align:left");
-    completed.style.textAlign = "right";
+    pg.classList.add("pg");
     
     title.textContent = this.title;
     author.textContent = "By, " + this.author;
@@ -114,6 +143,62 @@ book.prototype.display = function() {
     cardStack.appendChild(card);
 }
 
+// Creating all the cards
 for(let i = 0; i < myLibrary.length;i++){
     myLibrary[i].display();
 }
+
+// Adding the hover class to the card with the edit and remove functions
+const cards = cardStack.querySelectorAll(".card");
+cards.forEach(card => card.addEventListener("mouseenter", () => {
+    let cardHover = document.createElement("div");
+    const btnHover1 = document.createElement("button");
+    const btnHover2 = document.createElement("button");
+    btnHover1.textContent = "EDIT";
+    btnHover2.textContent = "REMOVE";
+    btnHover1.classList.add("btn-hover");
+    btnHover2.classList.add("btn-hover")
+    cardHover.appendChild(btnHover1);
+    cardHover.appendChild(btnHover2);
+    cardHover.classList.add("card-hover");
+    card.appendChild(cardHover);
+    btnHover2.addEventListener("click", () => {
+        let title = card.querySelector(".title").innerHTML;
+        let author = card.querySelector(".author").innerHTML;
+        myLibrary = myLibrary.filter(i => i.title != title && i.author != author);
+        author = author.slice(4);           
+        //Removing the data from local storage
+        for(let i = 0; i < n; i++){
+            let key = localStorage.key(i);
+            let value = localStorage.getItem(key);
+            let data = value.split("/");
+            if(title.toLowerCase() == data[0].toLowerCase() && author.toLowerCase() == data[1].toLowerCase()){
+                localStorage.removeItem(key);
+                location.reload();
+            }
+        }
+    })
+    btnHover1.addEventListener("click", () => {
+        enable = 0;
+        layout.appendChild(formWrapper);
+        let title = card.querySelector(".title").innerHTML;
+        let author = card.querySelector(".author").innerHTML;
+        author = author.slice(4);
+        let pages = card.querySelector(".pg").innerHTML;
+        pages = pages.slice(8);
+        let titleChange = form.querySelector("#title");
+        titleChange.value = title;
+        let authorChange = form.querySelector("#author");
+        authorChange.value = author;
+        let pagesChange = form.querySelector("#pages");
+        pagesChange.value = pages;   
+        edit = title + "|" + author;   
+    })
+})
+)
+
+
+cards.forEach(card => card.addEventListener("mouseleave", () => {
+    let cardHover = card.querySelector(".card-hover");
+    card.removeChild(cardHover);
+}))
